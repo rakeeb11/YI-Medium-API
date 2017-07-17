4b79565faddfae3e87c1cff130b97a8ee65a9bb0
Rakeeb Rajbhandari <rakeeb@Rakeebs-MacBook-Pro.local>
2017-07-17 05:49:34 +0545

    * initial commit

---
+++ Medium.go
@@ -0,0 +1,19 @@
+package MediumAPI
+
+import "fmt"
+
+func main() {
+	fmt.Println("Medium API")
+}
+
+
+type Posts struct {
+
+}
+
+type People struct {
+	id string
+	name string
+	username string
+	imageURL string
+}
